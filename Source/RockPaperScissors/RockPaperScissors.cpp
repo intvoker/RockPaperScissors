241e1e9f07bfa23c271d84f8eb07f02364eba178
intvoker <intvoker@gmail.com>
2023-02-03 21:34:28 -0800

    Initial commit

---
+++ Source/RockPaperScissors/RockPaperScissors.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RockPaperScissors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RockPaperScissors, "RockPaperScissors" );
---
+++ Source/RockPaperScissors/RockPaperScissorsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RockPaperScissorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROCKPAPERSCISSORS_API ARockPaperScissorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
